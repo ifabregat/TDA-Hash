81b89c1aa7b46373124db3dc5acd655d0ee9d29f
ifabregat <ifabregat@fi.uba.ar>
2024-11-09 00:51:23 -0300

    Implement a part of Hash

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,17 +1,47 @@
 #include "pa2m.h"
+#include "src/hash.h"
 
-void prueba_simple()
+void crearHash()
 {
-	int i = 14;
-	int j = 16;
-	pa2m_afirmar(i == j, "i es igual a j (%d == %d)", i, j);
-	pa2m_afirmar(2*i == 10, "2*i es igual a 2 (%d)", 2*i);
+	hash_t *hash = hash_crear(3);
+	pa2m_afirmar(hash != NULL, "Puedo crear un hash");
+	pa2m_afirmar(hash_cantidad(hash) == 0,
+		     "La cantidad de elementos en el hash es 0");
+	hash_destruir(hash);
+}
+
+void crearHashConCapacidadMenorA3()
+{
+	hash_t *hash = hash_crear(2);
+	pa2m_afirmar(hash != NULL,
+		     "Puedo crear un hash con capacidad menor a 3");
+	pa2m_afirmar(hash_cantidad(hash) == 0,
+		     "La cantidad de elementos en el hash es 0");
+	hash_destruir(hash);
+}
+
+void insertarElementoEnHash()
+{
+	hash_t *hash = hash_crear(3);
+	char *clave = "clave";
+	int valor = 1;
+	void *encontrado = NULL;
+	pa2m_afirmar(hash_insertar(hash, clave, &valor, &encontrado),
+		     "Puedo insertar un elemento en el hash");
+	pa2m_afirmar(hash_cantidad(hash) == 1,
+		     "La cantidad de elementos en el hash es 1");
+	pa2m_afirmar(encontrado == NULL, "No se encontró un elemento previo");
+	hash_destruir(hash);
 }
 
 int main()
 {
-	pa2m_nuevo_grupo("============== ??? ===============");
-	prueba_simple();
+	pa2m_nuevo_grupo("Pruebas de Creación de Hash");
+	crearHash();
+	printf("\n");
+	crearHashConCapacidadMenorA3();
+	pa2m_nuevo_grupo("Pruebas de Inserción de Elementos en Hash");
+	insertarElementoEnHash();
 
 	return pa2m_mostrar_reporte();
 }
--- src/hash.c
+++ src/hash.c
@@ -1,1 +1,117 @@
 #include "hash.h"
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct nodo {
+	char *clave;
+	void *valor;
+	struct nodo *siguiente;
+} nodo_t;
+
+struct hash {
+	nodo_t **tabla;
+	size_t cantidad;
+	size_t capacidad;
+	int (*hash_func)(char *, size_t);
+};
+
+int hash_func(char *clave, size_t capacidad)
+{
+	size_t hash = 0;
+
+	for (size_t i = 0; clave[i] != '\0'; i++)
+		hash += (size_t)clave[i];
+
+	return (int)(hash % capacidad);
+}
+
+hash_t *hash_crear(size_t capacidad_inicial)
+{
+	if (capacidad_inicial < 3)
+		capacidad_inicial = 3;
+
+	hash_t *hash = calloc(1, sizeof(hash_t));
+
+	if (hash == NULL)
+		return NULL;
+
+	hash->tabla = calloc(capacidad_inicial, sizeof(nodo_t *));
+
+	if (hash->tabla == NULL) {
+		free(hash);
+		return NULL;
+	}
+
+	hash->capacidad = capacidad_inicial;
+	hash->hash_func = hash_func;
+
+	return hash;
+}
+
+size_t hash_cantidad(hash_t *hash)
+{
+	return hash->cantidad;
+}
+
+bool hash_insertar(hash_t *hash, char *clave, void *valor, void **encontrado)
+{
+	if (hash == NULL || clave == NULL)
+		return false;
+
+	int indice = hash->hash_func(clave, hash->capacidad);
+
+	nodo_t *nodo = hash->tabla[indice];
+
+	while (nodo != NULL) {
+		if (strcmp(nodo->clave, clave) == 0) {
+			if (encontrado)
+				*encontrado = nodo->valor;
+
+			nodo->valor = valor;
+			return true;
+		}
+		nodo = nodo->siguiente;
+	}
+
+	nodo_t *nuevo_nodo = calloc(1, sizeof(nodo_t));
+
+	if (nuevo_nodo == NULL)
+		return false;
+
+	nuevo_nodo->clave = calloc(strlen(clave) + 1, sizeof(char));
+
+	if (nuevo_nodo->clave == NULL) {
+		free(nuevo_nodo);
+		return false;
+	}
+
+	strcpy(nuevo_nodo->clave, clave);
+
+	nuevo_nodo->valor = valor;
+	nuevo_nodo->siguiente = hash->tabla[indice];
+	hash->tabla[indice] = nuevo_nodo;
+
+	hash->cantidad++;
+
+	if (!encontrado)
+		*encontrado = NULL;
+
+	return true;
+}
+
+void hash_destruir(hash_t *hash)
+{
+	for (size_t i = 0; i < hash->capacidad; i++) {
+		nodo_t *nodo = hash->tabla[i];
+
+		while (nodo != NULL) {
+			nodo_t *nodo_aux = nodo->siguiente;
+			free(nodo->clave);
+			free(nodo);
+			nodo = nodo_aux;
+		}
+	}
+
+	free(hash->tabla);
+	free(hash);
+}
